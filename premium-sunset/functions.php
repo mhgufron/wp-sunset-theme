2d9ccb5fbd76c464aff56bc39ce78099207b6da6
mhgufron <mgufron223@gmail.com>
2018-04-30 14:44:20 +0700

    Error Done Lesson 8

--- premium-sunset/functions.php
+++
@@ -1,4 +0,0 @@
-<?php
-	
-require get_template_directory() . '/inc/function-admin.php';
-require get_template_directory() . '/inc/enqueue.php';
--- theme_sunset/inc/function-admin.php
+++ theme_sunset/inc/function-admin.php
@@ -63,6 +63,18 @@ function sunset_theme_options()
     echo "Activate and Deactivate specific Theme Support Options";
 }
 
+function sunset_post_formats(){
+    $options = get_option( 'post_formats' );
+    $formats = array( 'aside', 'galery', 'link', 'images', 'quote', 'status', 'video', 'audio', 'chat');
+    $output = '';
+    foreach ($formats as $format) {
+        $checked = ( @$options[$format] == 1 ) ? 'checked' : '';
+        $output .= '<label><input type="checkbox" id="' . $format . '" name="post_formats[' . $format . ']" value="1" ' . $checked . ' >' . $format . '</label><br />' ;
+    }
+    echo $output;
+}
+
+// Sidebar Option Function
 function sunset_sidebar_options()
 {
     echo "Customize your Sidebar Information";
