48ffff1473f56575fb6a6de7c64261f7f0a20947
mhgufron <mgufron223@gmail.com>
2018-05-17 11:18:03 +0700

    remove backup

--- sunsettheme (2)/assets/js/sunset.custom_css.js
+++
@@ -1,9 +0,0 @@
-jQuery(document).ready(function($) {
-    var updateCss = function() { $("#sunset_css").val( editor.getSession().getValue() ) };
-    $('#save-custom-css-form').submit( updateCss );
-
-});
-
-var editor = ace.edit("customCss");
-editor.setTheme("ace/theme/monokai");
-editor.session.setMode("ace/mode/css");
