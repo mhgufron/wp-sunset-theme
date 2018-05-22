ac41477fae3ae42dede36c2204aee88f77c0c740
mhgufron <mgufron223@gmail.com>
2018-05-22 15:36:51 +0700

    fix bootstrap carousel notworking on loadmore

--- sunsettheme/assets/js/sunset.js
+++ sunsettheme/assets/js/sunset.js
@@ -5,21 +5,26 @@ jQuery(document).ready( function($) {
     revealPost();
 
     /* Variable declaration */
-    var carousel    = '.sunset-carousel-thumb';
     var last_scroll = 0;
 
     /* Carousel functions */
-    sunset_get_bs_thumbs( carousel );
+    $(document).on('click', '.sunset-carousel-thumb', function () {
+        var id = $('#' + $(this).attr('id'));
+        $(id).on('slid.bs.carousel', function () {
+            sunset_get_bs_thumbs(id);
+        })
+    })
 
-    $( carousel ).on('slid.bs.carousel', function() {
-        sunset_get_bs_thumbs( carousel );
-    });
+    $(document).on('mouseover', '.sunset-carousel-thumb', function () {
+        var id = $('#' + $(this).attr('id'));
+        sunset_get_bs_thumbs(id);
+    })
 
-    function sunset_get_bs_thumbs( carousel ) {
-        var nextThumb = $('.item.active').find('.next-image-preview').data('image');
-        var prevThumb = $('.item.active').find('.prev-image-preview').data('image');
-        $(carousel).find('.carousel-control.right').find('.thumbnail-container').css({ 'background-image': 'url(' + nextThumb + ')'});
-        $(carousel).find('.carousel-control.left').find('.thumbnail-container').css({ 'background-image': 'url(' + prevThumb + ')'});
+    function sunset_get_bs_thumbs( id ) {
+        var nextThumb = $(id).find('.item.active').find('.next-image-preview').data('image');
+        var prevThumb = $(id).find('.item.active').find('.prev-image-preview').data('image');
+        $(id).find('.carousel-control.right').find('.thumbnail-container').css({ 'background-image': 'url(' + nextThumb + ')'});
+        $(id).find('.carousel-control.left').find('.thumbnail-container').css({ 'background-image': 'url(' + prevThumb + ')'});
     }
 
     /* Scroll Function */
