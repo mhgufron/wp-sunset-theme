b21b0cf8cc7dd060ceb8e32e7ebe95ab99199a94
mhgufron <mgufron223@gmail.com>
2018-05-22 10:48:05 +0700

    Loop Archive page Lesson 36 fix

--- sunsettheme/archive.php
+++ sunsettheme/archive.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Index page
+ * Archive page
  *
  * @package sunsettheme
  * @since 1.0
@@ -10,31 +10,34 @@
 get_header();?>
 
 <div id="primary" class="content-area">
+
     <main id="main" class="site-main" role="main">
 
-        <header class="archive-header text-center">
-            <?php the_archive_title('<h1 class="page-title">', '</h1>'); ?>
-        </header>
+    <header class="archive-header text-center">
+        <?php the_archive_title('<h1 class="page-title">', '</h1>') ?>
+    </header>
 
         <?php if ( is_paged() ): ?>
-
             <div class="container text-center container-load-previous">
-                <a class="btn-sunset-load sunset-load-more" data-prev="1" data-archive="<?php echo sunset_grab_current_uri(); ?>" data-page="<?php echo sunset_check_paged(1); ?>" data-url="<?php echo admin_url( 'admin-ajax.php' ) ?>">
+                <a class="btn-sunset-load sunset-load-more" data-prev="1" data-archive="<?php echo $_SERVER[ 'REQUEST_URI' ] ?>" data-page="<?php echo sunset_check_paged(1); ?>" data-url="<?php echo admin_url( 'admin-ajax.php' ) ?>">
                     <span class="sunset-icon icon-loading"></span>
                     <span class="text">Load Previous</span>
                 </a>
             </div><!-- .container -->
-
         <?php endif; ?>
 
-        <div class="container sunset-posts-container">
+        <div class="container sunset-post-container">
 
-            <?php if ( have_posts() ) :
+            <?php
 
-                    echo '<div class="page-limit" data-page="' . $_SERVER["REQUEST_URI"] . '"/ >';
+                if ( have_posts() ) :
+
+                    echo '<div class="page-limit" data-page="' . $_SERVER[ 'REQUEST_URI' ] . '" >';
 
                     while ( have_posts() ): the_post();
 
+                        // $class = 'reveal';
+                        // set_query_var( 'post-class', $class );
                         get_template_part( 'template-parts/content', get_post_format() );
 
                     endwhile;
@@ -47,8 +50,10 @@ get_header();?>
 
         </div><!-- .container -->
 
+        </form>
+
         <div class="container text-center">
-            <a class="btn-sunset-load sunset-load-more" data-page="<?php echo sunset_check_paged(1); ?>" data-archive="<?php echo sunset_grab_current_uri(); ?>" data-url="<?php echo admin_url( 'admin-ajax.php' ) ?>">
+            <a class="btn-sunset-load sunset-load-more" data-page="<?php echo sunset_check_paged(1); ?>" data-archive="<?php echo $_SERVER[ 'REQUEST_URI' ] ?>" data-url="<?php echo admin_url( 'admin-ajax.php' ) ?>">
                 <span class="sunset-icon icon-loading"></span>
                 <span class="text">Load More</span>
             </a>
